807871c72c270977466270d7921d5cf6e12ddf4d
RetaxMaster <retaxmaster@hotmail.com>
2022-02-24 15:47:01 -0600

    Clase 16 otros tipos de inputs

--- tipos-de-inputs/formulario.php
+++ tipos-de-inputs/formulario.php
@@ -11,29 +11,29 @@
     <form action="server.php" method="post" enctype="multipart/form-data">
 
         <!-- Input simple -->
-        <!-- <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre"> -->
+        <label for="nombre">Nombre:</label>
+        <input type="text" name="nombre" id="nombre">
 
         <!-- Arreglos -->
-        <!-- <label>Personas:</label>
+        <label>Personas:</label>
         <input type="text" name="personas[]">
         <input type="text" name="personas[]">
-        <input type="text" name="personas[]"> -->
+        <input type="text" name="personas[]">
 
         <!-- Arreglos asociativos -->
-        <!-- <label>Nombre de la persona:</label>
+        <label>Nombre de la persona:</label>
         <input type="text" name="persona[nombre]">
 
         <label>Correo de la persona:</label>
         <input type="email" name="persona[email]">
 
         <label>Edad de la persona:</label>
-        <input type="number" name="persona[edad]"> -->
+        <input type="number" name="persona[edad]">
 
         <!-- Checkbox -->
-        <!-- <input type="checkbox" name="list1">
+        <input type="checkbox" name="list1">
         <input type="checkbox" name="list2" value="Este valor fue clicado">
-        <input type="checkbox" name="list3"> -->
+        <input type="checkbox" name="list3">
 
         <!-- Radios -->
         <label for="mexico">México</label>
@@ -44,6 +44,10 @@
 
         <label for="peru">Perú</label>
         <input type="radio" name="pais" value="Perú" id="peru">
+
+        <!-- Múltiples archivos -->
+        <label for="galeria">Carga tus imágenes:</label>
+        <input type="file" multiple name="galeria[]" id="galeria">
 
         <button type="submit">Mandar formulario</button>
 
--- tipos-de-inputs/server.php
+++ tipos-de-inputs/server.php
@@ -8,4 +8,5 @@ var_dump($_POST["list1"]);
 var_dump($_POST["list2"]);
 var_dump($_POST["list3"]);
 var_dump($_POST["pais"]);
+var_dump($_FILES["galeria"]);
 echo "</pre>";
