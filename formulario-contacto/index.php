5a4b723483475cd6854b5ea9665552c1976dbfa4
RetaxMaster <retaxmaster@hotmail.com>
2022-02-24 17:39:06 -0600

    Clase 21 Logica de recepcion

--- formulario-contacto/index.php
+++ formulario-contacto/index.php
@@ -1,3 +1,33 @@
+<?php
+
+function validate($name, $email, $subject, $message, $form) {
+    return !empty($name) && !empty($email) && !empty($subject) && !empty($message);
+}
+
+$status = "";
+
+if ( isset($_POST["form"]) ) {
+
+    if ( validate(...$_POST) ) {
+        
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+
+        // Mandar el correo
+
+        $status = "success";
+
+    }
+    else {
+        $status = "danger";
+    }
+   
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,15 +39,24 @@
 </head>
 <body>
 
+    <?php if($status == "danger"): ?>
+    
     <div class="alert danger">
         <span>Surgió un problema</span>
     </div>
+    
+    <?php endif; ?>
+    
 
+    <?php if($status == "success"): ?>
+    
     <div class="alert success">
         <span>¡Mensaje enviado con éxito!</span>
     </div>
+    
+    <?php endif; ?>
 
-    <form action="#">
+    <form action="./" method="POST">
 
         <h1>¡Contáctanos!</h1>
 
@@ -42,7 +81,7 @@
         </div>
 
         <div class="button-container">
-            <button type="submit">Enviar</button>
+            <button name="form" type="submit">Enviar</button>
         </div>
 
         <div class="contact-info">
